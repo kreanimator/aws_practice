43d62479f3da816d3cfd0fd933e0ee7ebb0a8215
kreanimator <backin.v@yandex.ru>
2024-04-30 00:17:04 +0300

    Added user input to the function

--- amplify_react_app/app/story_sparker.py
+++ amplify_react_app/app/story_sparker.py
@@ -1,23 +1,40 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv('key.env')
 
-api_key = os.getenv('OPENAI_API_KEY')
 
-client = OpenAI(api_key=api_key)
+def main():
+
+    print("Running Story Sparker")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
+    print(f"User input: {user_input}")
+    generate_story(user_input)
+
+def generate_story(prompt: str):
+
+    api_key = os.getenv('OPENAI_API_KEY')
+    client = OpenAI(api_key=api_key)
+
+    enriched_prompt = f"Generate a short story or anecdote about {prompt}: Keep it short, no more than 200 tokens"
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=enriched_prompt,
+        max_tokens=200
+    )
+
+    story_text: str = response.choices[0].text
+    story_text.strip()
+    last_char = story_text[-1]
+    if last_char not in {".", "!", "?"}:
+        story_text += "..."
+    print(story_text)
 
 
-subject = 'Lost in the forest at night.'
-prompt = f'Generate a cool story for "{subject}"'
-
-# Generate text completion
-response = client.completions.create(
-    model="davinci-002",
-    prompt=prompt,
-    max_tokens=32
-)
-
-# Print the generated response
-print(response.choices[0].text.strip())
+if __name__ == "__main__":
+    main()
