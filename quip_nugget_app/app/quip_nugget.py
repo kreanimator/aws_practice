38947efd08c9689a16d36e84a9c69316665f2a96
kreanimator <backin.v@yandex.ru>
2024-04-30 08:19:59 +0300

    Added user input to the function

---
+++ quip_nugget_app/app/quip_nugget.py
@@ -0,0 +1,83 @@
+import os
+from typing import List
+
+from openai import OpenAI
+from dotenv import load_dotenv
+import argparse
+import re
+
+load_dotenv('key.env')
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=api_key)
+
+
+def main():
+
+    print("Running Quip Nugget")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
+    print(f"User input: {user_input}")
+    joke_result = generate_joke(user_input)
+    fact_result = generate_fact(user_input)
+    keyword_result = generate_keywords(user_input)
+    print(joke_result)
+    print(fact_result)
+    print(keyword_result)
+
+
+def generate_joke(prompt: str) -> str:
+
+    enriched_prompt = f"Generate a joke about {prompt}:"
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=enriched_prompt,
+        max_tokens=32
+    )
+
+    story_text: str = response.choices[0].text.strip()
+    last_char = story_text[-1]
+    if last_char not in {".", "!", "?"}:
+        story_text += "..."
+
+    return story_text
+
+
+def generate_fact(prompt: str) -> str:
+
+    enriched_prompt = f"Generate a fan fact about {prompt}:"
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=enriched_prompt,
+        max_tokens=32
+    )
+
+    fact_text: str = response.choices[0].text.strip()
+    last_char = fact_text[-1]
+    if last_char not in {".", "!", "?"}:
+        fact_text += "..."
+
+    return fact_text
+
+
+def generate_keywords(prompt: str) -> List[str]:
+
+    enriched_prompt = f"Generate related branding keywords for {prompt}:"
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=enriched_prompt,
+        max_tokens=32
+    )
+
+    keywords_text = response.choices[0].text.strip()
+    keywords_text = re.sub(r'\d+\.', '', keywords_text)
+    keywords_text = re.sub(r'\s+', ' ', keywords_text)
+    keywords_list = keywords_text.strip().split()
+    keywords_list = [k.lower() for k in keywords_list]
+
+    return keywords_list
+
+
+if __name__ == "__main__":
+    main()
