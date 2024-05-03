eec309b27d561bf2653e8e8f64411697b2a571e8
kreanimator <backin.v@yandex.ru>
2024-05-04 00:55:37 +0300

    Fixed description for keywords

--- fe_quip_nugget/quip-nugget/components/results.tsx
+++ fe_quip_nugget/quip-nugget/components/results.tsx
@@ -9,57 +9,71 @@ interface ResultsProps{
     }
 
 const Results: React.FC<ResultsProps> = (props) => {
+//     const keywordElements = props.keywords.map((keyword, index) => (
+//         <div
+//             key={index}
+//             className="bg-gradient-to-r from-green-300 to-emerald-400 p-1 text-white shadowed-text px-2 text-sm rounded-md"
+//         >
+//             #{keyword}
+//
+//         </div>
+//     ));
+      const keywordElements = [];
 
-         const keywordElements = [];
-          for (let i = 0; i < props.keywords.length; i++) {
-            const element = (
-              <div
-                key={i}
-                className="bg-gradient-to-r from-green-300 to-emerald-400 p-1 text-white shadowed-text px-2 text-sm rounded-md"
-
-              >
-                #{props.keywords[i]}
-              </div>
-            );
-            keywordElements.push(element);
-          }
-       const keywordElementsHolder = (
-        <div className="flex flex-wrap gap-2">{keywordElements}</div>
-        );
-
-       const resultSection = (label: string, body: any, color: string) => {
-        return (
-          <div className={`${color} p-4 my-3 rounded-md`}>
-            <div className="text-white text-sm font-bold mb-1" >{label}</div>
-            <div>{body}</div>
+      for (let i = 0; i < props.keywords.length; i++) {
+        const element = (
+          <div
+            key={i}
+            className="bg-gradient-to-r from-green-300 to-emerald-400 p-1 text-white shadowed-text px-2 text-sm rounded-md"
+          >
+            #{props.keywords[i]}
           </div>
         );
-        };
+        keywordElements.push(element);
+      }
 
-        const shareContent = `Check out this joke: ${props.joke}\n\nCheck out this fun fact: ${props.fact}\n\n${props.keywords.map(keyword => `#${keyword.trim()}`).join(' ')}`;
+    const keywordElementsHolder = (
+        <div className="flex flex-wrap gap-2">{keywordElements}</div>
+    );
+
+    const resultSection = (label: string, body: any, color: string) => {
+        return (
+            <div className={`${color} p-4 my-3 rounded-md`}>
+                <div className="text-white text-sm font-bold mb-1">{label}</div>
+                <div>{body}</div>
+            </div>
+        );
+    };
 
 
-       return (
-               <>
-                  <div className="mb-6">
-                    {resultSection(
-                      "Your funny input",
-                      <div className="text-lg font-bold">{props.prompt}</div>, "bg-emerald-700"
-                    )}
-                    {resultSection("Joke", props.joke, "bg-emerald-700")}
-                    {resultSection("Funfact", props.fact, "bg-emerald-700")}
-                    {resultSection("Keywords", keywordElementsHolder, "bg-emerald-700")}
-                  </div>
-                  <Share description={shareContent} />
+    const processedKeywords = props.keywords.map(keyword => `#${keyword.trim().replace(/,/g, '')}`).join(' ');
+    console.log(processedKeywords)
 
-                  <button
-                    className="bg-gradient-to-r from-emerald-400
+    const shareContent = `Check out this joke: ${props.joke}\n\nCheck out this fun fact: ${props.fact}\n\n${processedKeywords}`;
+
+    console.log(shareContent)
+    return (
+        <>
+            <div className="mb-6">
+                {resultSection(
+                    "Your funny input",
+                    <div className="text-lg font-bold">{props.prompt}</div>,
+                    "bg-emerald-700"
+                )}
+                {resultSection("Joke", props.joke, "bg-emerald-700")}
+                {resultSection("Funfact", props.fact, "bg-emerald-700")}
+                {resultSection("Keywords", keywordElementsHolder, "bg-emerald-700")}
+            </div>
+            <Share description={shareContent} />
+
+            <button
+                className="bg-gradient-to-r from-emerald-400
                     to-emerald-800 disabled:opacity-50 w-full p-2 rounded-md text-lg"
-                    onClick={props.onBack}
-                  >
-                    New joke
-                  </button>
-                </>
+                onClick={props.onBack}
+            >
+                New joke
+            </button>
+        </>
     );
 };
 
