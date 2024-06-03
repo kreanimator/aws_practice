4b443f75e2c2983777865d14bffa3f48994e9808
kreanimator <backin.v@yandex.ru>
2024-06-03 11:06:49 +0300

    Added updated function which updates usage count and adds items to the list in ddb

--- functions/quip_nugget_app/src/app.py
+++ functions/quip_nugget_app/src/app.py
@@ -37,6 +37,8 @@ class Processor(SoswProcessor):
                 'user_input':  'S',
                 'created_at':  'N',
                 'usage_count': 'N',
+                'jokes':       'L',
+                'facts':       'L',
             },
             'required_fields': ['id', 'user_input', 'created_at'],
         },
@@ -59,13 +61,16 @@ class Processor(SoswProcessor):
         logger.info(f"User input: {user_input}")
         try:
             if self.validate_input(user_input):
-                result = self.generate_response(event, user_input)
-                logger.info(f"Result: {result}")
-                if result:
-                    response = self.make_entry_from_event(event)
-                    self.find_prompt(response, result)
+                response = self.generate_response(event, user_input)
+                logger.info(f"Response: {response}")
+                if response:
+                    entry = self.make_entry_from_event(event, response)
+                    if not self.find_prompt(entry, response):
+                        self.send_event_to_ddb(entry)
+                    else:
+                        self.update_data_in_ddb(entry, response)
 
-                return result
+                return response
             else:
                 input_length = self.config['max_input_length']
                 return f'Your input is more than {input_length}'
@@ -137,7 +142,7 @@ class Processor(SoswProcessor):
         return joke_text
 
 
-    def make_entry_from_event(self, event):
+    def make_entry_from_event(self, event, response):
 
         rec_id = str(uuid.uuid4())
 
@@ -146,6 +151,10 @@ class Processor(SoswProcessor):
 
         user_input = event.get('queryStringParameters', {}).get('user_input')
         meta = event.get('requestContext', {}).get('http')
+        jokes = []
+        facts = []
+        jokes.append(response['joke'])
+        facts.append(response['fact'])
 
         entry = {
             'id':          rec_id,
@@ -153,8 +162,8 @@ class Processor(SoswProcessor):
             'user_input':  user_input,
             'usage_count': 1,
             'meta':        meta,
-            'jokes':       [],
-            'facts':       []
+            'jokes':       jokes,
+            'facts':       facts
         }
 
         entry = self.validate_entry(entry)
@@ -213,27 +222,124 @@ class Processor(SoswProcessor):
             keys = {'user_input': user_input}
             comparisons = {'user_input': '='}
 
-            db_result = self.dynamo_db_client.get_by_query(keys=keys, table_name=table_name, comparisons=comparisons,
-                                                           index_name=index_name)
-            logger.debug(f"Prompt result: {db_result}")
-            if db_result:
-                logger.debug("Found existing entry by user_input: %s", db_result)
-                existing_entry = db_result[0]
-                usage_count = int(existing_entry.get('usage_count', 0))
-                usage_count += 1
-                existing_entry['usage_count'] = usage_count
+            is_found = self.dynamo_db_client.get_by_query(keys=keys, table_name=table_name, comparisons=comparisons,
+                                                          index_name=index_name)
+            logger.debug(f"Prompt result: {is_found}")
+            if is_found:
+                logger.debug("Found existing entry by user_input: %s", is_found)
+                return True
 
-                logger.info(f"Result data: {response}")
-                if 'joke' in response:
-                    existing_entry.setdefault('jokes', []).append(response['joke'])
+        return False
 
-                if 'fact' in response:
-                    existing_entry.setdefault('facts', []).append(response['fact'])
 
-                entry.update(existing_entry)
-        # self.dynamo_db_client.update(entry)
-        logger.info(f"Sucessfully updated entry: {entry}")
-        self.send_event_to_ddb(entry)
+    def update_data_in_ddb(self, entry: dict, response: dict) -> None:
+        try:
+            user_input = entry.get('user_input')
+            query_result = self.dynamo_db_client.get_by_query(
+                keys={'user_input': user_input},
+                table_name=self.config['dynamo_db_config']['table_name'],
+                index_name='user_input'
+            )
+
+            if query_result:
+                existing_item = query_result[0]
+                item_id = existing_item['id']
+
+                scan_query = {
+                    'filter_expression': f'id = {item_id}',
+                    'table_name':        self.config['dynamo_db_config']['table_name'],
+                    'index_name':        'user_input'
+                }
+
+                existing_items = list(self.get_by_scan_generator(**scan_query))
+                logger.info(f"Existing items {existing_items}")
+
+                if existing_items:
+                    existing_item = existing_items[0]
+
+                    usage_count = int(existing_item.get('usage_count', 0)) + 1
+                    existing_item['usage_count'] = usage_count
+
+                    jokes = existing_item.get('jokes', [])
+                    facts = existing_item.get('facts', [])
+                    logger.info(f"Existing jokes: {jokes} Existing facts: {facts}")
+                    if 'joke' in response:
+                        jokes.append(response['joke'])
+                    if 'fact' in response:
+                        facts.append(response['fact'])
+
+                    existing_item['jokes'] = jokes
+                    existing_item['facts'] = facts
+                    logger.info(
+                        f"After append Existing jokes: {existing_item.get('jokes', [])} Existing facts: {existing_item.get('facts', [])}")
+
+                    attributes_to_update = {
+                        'jokes':       existing_item['jokes'],
+                        'facts':       existing_item['facts'],
+                        'usage_count': existing_item['usage_count']
+                    }
+
+                    self.dynamo_db_client.update(
+                        keys={'id': item_id},
+                        attributes_to_update=attributes_to_update,
+                        table_name=self.config['dynamo_db_config']['table_name']
+                    )
+                    logger.info(f"Successfully updated entry with id '{item_id}': {entry}")
+                else:
+                    logger.warning(f"No full entry found with id '{item_id}' in the table.")
+            else:
+                logger.warning(f"No entry found with user_input '{user_input}' in the index.")
+        except Exception as e:
+            logger.error(f"Error updating entry: {e}")
+
+
+    def get_by_scan_generator(self, attrs=None, table_name=None, index_name=None, fetch_all_fields=None,
+                              consistent_read=None, filter_expression=None):
+        """
+        Scans a table. Don't use this method if you want to select by keys. It is SLOW compared to get_by_query.
+        Careful - don't make queries of too many items, this could run for a long time.
+        Same as get_by_scan, but yields parts of the results.
+        """
+        ddbc = self.dynamo_db_client
+        fetch_all_fields = fetch_all_fields if fetch_all_fields is not None else False
+
+        response_iterator = self._build_scan_iterator(attrs, table_name, index_name, consistent_read, filter_expression)
+        logger.debug(f"Paginating response_iterator: {response_iterator}")
+
+        for page in response_iterator:
+            self.stats['dynamo_scan_queries'] += 1
+            logger.debug(f"Processing page with {len(page['Items'])} rows")
+            for row in page['Items']:
+                yield ddbc.dynamo_to_dict(row, fetch_all_fields=fetch_all_fields)
+
+
+    def _build_scan_iterator(self, attrs=None, table_name=None, index_name=None, consistent_read=None,
+                             filter_expression=None):
+        ddbc = self.dynamo_db_client
+        table_name = ddbc._get_validate_table_name(table_name)
+
+        query_args = {
+            'TableName':                 table_name,
+            'Select':                    'ALL_ATTRIBUTES',
+            'ExpressionAttributeValues': {}
+        }
+
+        if filter_expression:
+            expr, values = ddbc._parse_filter_expression(filter_expression)
+            query_args['FilterExpression'] = expr
+            query_args['ExpressionAttributeValues'].update(values)
+
+        if consistent_read is not None:
+            logger.debug(f"Forcing ConsistentRead to: {consistent_read}")
+            query_args['ConsistentRead'] = consistent_read
+
+        logger.debug(f"Scanning DynamoDB with query args: {query_args}")
+
+        paginator = ddbc.dynamo_client.get_paginator('scan')
+        response_iterator = paginator.paginate(**query_args)
+
+        return response_iterator
+
 
     def send_event_to_sns(self, event):
         logger.info("Sending to SNS event: %s", event)
